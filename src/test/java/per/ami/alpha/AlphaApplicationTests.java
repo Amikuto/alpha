af962ff8ac3d2292505dc643258a5ffcc32a2ea4
Damir Kurtaev <damir.kurtaev@yandex.ru>
2021-12-15 17:40:32 +0300

    Project started

---
+++ src/test/java/per/ami/alpha/AlphaApplicationTests.java
@@ -0,0 +1,13 @@
+package per.ami.alpha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
