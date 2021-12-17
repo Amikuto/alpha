b9a553cff24756743af4624e129e9be0278ade59
Damir Kurtaev <damir.kurtaev@yandex.ru>
2021-12-17 16:05:50 +0300

    Config(settings) and task added

--- src/main/java/per/ami/alpha/AlphaApplication.java
+++ src/main/java/per/ami/alpha/AlphaApplication.java
@@ -1,10 +1,25 @@
 package per.ami.alpha;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class AlphaApplication {
+
+    @Autowired
+    private AppConfig appConfig;
+
+    @PostConstruct
+    void print() {
+        System.out.println("using environment: " + appConfig.getEnvironment());
+        System.out.println("name: " + appConfig.getName());
+        System.out.println("enabled:" + appConfig.isEnabled());
+        System.out.println("resources: " + appConfig.getResources());
+        System.out.println("currencies: " + appConfig.getCurrencies());
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AlphaApplication.class, args);
---
+++ src/main/java/per/ami/alpha/AppConfig.java
@@ -0,0 +1,61 @@
+package per.ami.alpha;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Configuration
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "proj")
+public class AppConfig {
+
+    private String name;
+    private String environment;
+    private boolean enabled;
+    private List<String> resources = new ArrayList<>();
+    private Map<String, String> currencies;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEnvironment() {
+        return environment;
+    }
+
+    public void setEnvironment(String environment) {
+        this.environment = environment;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public List<String> getResources() {
+        return resources;
+    }
+
+    public void setResources(List<String> resources) {
+        this.resources = resources;
+    }
+
+    public Map<String, String> getCurrencies() {
+        return currencies;
+    }
+
+    public void setCurrencies(Map<String, String> currencies) {
+        this.currencies = currencies;
+    }
+}
