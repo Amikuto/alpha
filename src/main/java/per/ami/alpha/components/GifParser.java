a4e9bb9f8d270220edc15278e96cfa77058d163e
Damir Kurtaev <damir.kurtaev@yandex.ru>
2021-12-19 21:35:17 +0300

    Raw done

--- src/main/java/per/ami/alpha/components/GifParser.java
+++ src/main/java/per/ami/alpha/components/GifParser.java
@@ -1,7 +1,16 @@
 package per.ami.alpha.components;
 
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GifParser {
+
+    public static String parse(String stringToParse) {
+        JSONObject jsonObject = new JSONObject(stringToParse);
+        JSONObject jsonObject1 = jsonObject.getJSONObject("data");
+        JSONObject jsonObject2 = jsonObject1.getJSONObject("images");
+        JSONObject jsonObject3 = jsonObject2.getJSONObject("downsized_large");
+        return (jsonObject3.get("url").toString());
+    }
 }
