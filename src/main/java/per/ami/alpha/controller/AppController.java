9f3448b59e2af6318fd8e32e14526f6483ef49f8
Damir Kurtaev <damir.kurtaev@yandex.ru>
2021-12-23 00:33:54 +0300

    AppController: закоменчен ненужный код ApiServiceImpl: добавлена логика приложения - дата форматтер, получение валюты и установка типа гиффки по сравнению валюты за день

--- src/main/java/per/ami/alpha/controller/AppController.java
+++ src/main/java/per/ami/alpha/controller/AppController.java
@@ -11,50 +11,35 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-//@RestController
-//@RequiredArgsConstructor
-//public class AppController {
-//
-//    @Autowired
-//    CurrencyService currencyService;
-//
-//    @GetMapping(value = "/info")
-//    public ResponseEntity<String> showGif(@RequestParam("currency") String currency) throws IOException {
-//        return ResponseEntity.ok().body("<img src=" + currencyService.getGif(currency) + " alt=\"Funny image\">");
-//    }
-//
-//
-//}
-
 @RestController
 public class AppController {
 
-    @Autowired
-    CurrencyServiceClient currencyServiceClient;
-
-    @Autowired
-    GifServiceClient gifServiceClient;
+//    @Autowired
+//    CurrencyServiceClient currencyServiceClient;
+//
+//    @Autowired
+//    GifServiceClient gifServiceClient;
 
     @Autowired
     ApiService apiService;
 
-    @GetMapping("/getLastCurrency")
-    public ResponseEntity<?> getLastCurrency() {
-        return ResponseEntity.ok(currencyServiceClient.getLastCurrencies());
-    }
-
-    @GetMapping("/getYesterdayCurrency")
-    public ResponseEntity<?> getYesterdayCurrency() {
-        DateTimeFormatter formmat = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
-        LocalDateTime ldt2 = LocalDateTime.now().minusDays(1);
-        String date = formmat.format(ldt2);
-        return ResponseEntity.ok(currencyServiceClient.getYesterdayCurrencies(date));
-    }
-
-    @GetMapping("/getGif")
-    public ResponseEntity<?> getGif() {
-        return ResponseEntity.ok(gifServiceClient.getGif("rich"));
-    }
+//    @GetMapping("/getLastCurrency")
+//    public ResponseEntity<?> getLastCurrency() {
+//        return ResponseEntity.ok(currencyServiceClient.getLastCurrencies());
+//    }
+//
+//    @GetMapping("/getYesterdayCurrency")
+//    public ResponseEntity<?> getYesterdayCurrency() {
+//        DateTimeFormatter formmat = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+//        LocalDateTime ldt2 = LocalDateTime.now().minusDays(1);
+//        String date = formmat.format(ldt2);
+//        return ResponseEntity.ok(currencyServiceClient.getYesterdayCurrencies(date));
+//    }
+//
+//    @GetMapping("/getGif")
+//    public ResponseEntity<?> getGif() {
+//        return ResponseEntity.ok(gifServiceClient.getGif("rich"));
+//    }
 
     @GetMapping("/api/info")
     public ResponseEntity<?> getInfoGif(@RequestParam("currency") String currency) {
--- src/main/java/per/ami/alpha/service/impl/ApiServiceImpl.java
+++ src/main/java/per/ami/alpha/service/impl/ApiServiceImpl.java
@@ -7,55 +7,12 @@ import per.ami.alpha.client.GifServiceClient;
 import per.ami.alpha.pojo.dto.InfoPage;
 import per.ami.alpha.service.ApiService;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 @Service
 public class ApiServiceImpl implements ApiService {
-
-//    @Autowired
-//    AppConfig appConfig;
-//
-//    @Autowired
-//    HttpPoster httpPoster;
-//
-//    @Autowired
-//    CurrencyParser currencyParser;
-//
-//    @Override
-//    public String getGif(String currency) throws IOException {
-//
-////        System.out.println(appConfig.getCurrencyinfo().getNames().get(currency));
-//
-//
-//        DateTimeFormatter formmat = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
-//        LocalDateTime ldt2 = LocalDateTime.now().minusDays(1);
-////        String formatter2 = formmat.format(ldt2);
-//
-//        String type = "latest.json";
-//        String date = formmat.format(ldt2);
-//        String type2 = "historical/" + date + ".json";
-//
-//
-//        String yest = appConfig.getApilinks().get("currency") + type2 + appConfig.getApikey().get("currency");
-//        String today = appConfig.getApilinks().get("currency") + type + appConfig.getApikey().get("currency");
-////        System.out.println(yest);
-////        System.out.println(today);
-//
-//        String tag;
-//        float currYesterday = CurrencyParser.parse(httpPoster.sendPost(yest), currency);
-//        float currToday = CurrencyParser.parse(httpPoster.sendPost(today), currency);
-//
-//        System.out.println(currYesterday + " " +currToday);
-//
-//        if (currYesterday < currToday) {
-//            tag = "rich";
-//        } else {
-//            tag = "broke";
-//        }
-//
-//        String linkGif = appConfig.getApilinks().get("gif") + appConfig.getApikey().get("gif") + "&tag=" + tag;
-//        return GifParser.parse(httpPoster.sendPost(linkGif));
-//    }
-
-
 
     @Autowired
     CurrencyServiceClient currencyServiceClient;
@@ -66,15 +23,29 @@ public class ApiServiceImpl implements ApiService {
     @Override
     public InfoPage getPage(String currency) {
         String gifType = "rich";
-        String gifUrl = String.valueOf(gifServiceClient.getGif("rich").getData().getImages().getOriginal().get("url"));
+        String gifUrl = gifServiceClient.getGif("rich").getData().getImages().getOriginal().get("url");
         InfoPage infoPage = InfoPage.builder().url(gifUrl).type(gifType).build();
         return infoPage;
     }
 
     @Override
     public String getStringPage(String currency) {
-        String gifType = "rich";
-        String gifUrl = String.valueOf(gifServiceClient.getGif("rich").getData().getImages().getOriginal().get("url"));
+
+        DateTimeFormatter currencyApiDF = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.ENGLISH);
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+
+        Float todayCurr = currencyServiceClient.getLastCurrencies().getRates().get(currency);
+        Float yestCurr = currencyServiceClient.getYesterdayCurrencies(currencyApiDF.format(yesterday)).getRates().get(currency);
+
+
+        String gifType;
+        if (yestCurr < todayCurr) {
+            gifType = "rich";
+        } else {
+            gifType = "broke";
+        }
+
+        String gifUrl = gifServiceClient.getGif(gifType).getData().getImages().getOriginal().get("url");
         InfoPage infoPage = InfoPage.builder().url(gifUrl).type(gifType).build();
         return infoPage.toString();
     }
