6df337381adc97fe0ef2f71ea7812fdb1dc04b3f
Damir Kurtaev <damir.kurtaev@yandex.ru>
2021-12-23 00:31:43 +0300

    Gif pojo class: теперь в нем вложенные классы

--- src/main/java/per/ami/alpha/pojo/dto/Gif.java
+++ src/main/java/per/ami/alpha/pojo/dto/Gif.java
@@ -1,8 +1,6 @@
 package per.ami.alpha.pojo.dto;
 
-import lombok.Builder;
 import lombok.Data;
-import lombok.ToString;
 
 import java.util.Map;
 
@@ -10,54 +8,57 @@ import java.util.Map;
 public class Gif {
     private GifData data;
     private Meta meta;
+
+    @Data
+    public class Meta {
+        private String msg;
+        private long status;
+        private String response_id;
+    }
+
+    @Data
+    public class GifData {
+        private String type;
+        private String id;
+        private String url;
+        private String slug;
+        private String bitly_gif_url;
+        private String bitly_url;
+        private String embed_url;
+        private String username;
+        private String source;
+        private String title;
+        private String rating;
+        private String content_url;
+        private String source_tld;
+        private String source_post_url;
+        private long is_sticker;
+        private String import_datetime;
+        private String trending_datetime;
+        private GifImages images;
+        private User user;
+
+        @Data
+        public class GifImages {
+            private Map<String, String> original;
+        }
+
+        @Data
+        public class User {
+            private String avatarURL;
+            private String bannerImage;
+            private String bannerURL;
+            private String profileURL;
+            private String username;
+            private String displayName;
+            private String description;
+            private boolean isVerified;
+            private String websiteURL;
+            private String instagramURL;
+        }
+    }
 }
 
-@Data
-class Meta {
-    private String msg;
-    private long status;
-    private String response_id;
-}
-
-@Data
-class User {
-    private String avatarURL;
-    private String bannerImage;
-    private String bannerURL;
-    private String profileURL;
-    private String username;
-    private String displayName;
-    private String description;
-    private boolean isVerified;
-    private String websiteURL;
-    private String instagramURL;
-}
-
-
-//            private FixedHeight fixedHeightDownsampled;
-//            private The480_WStill downsizedStill;
-//            private The480_WStill fixedHeightStill;
-//            private The480_WStill downsizedMedium;
-//            private The480_WStill downsized;
-//            private The480_WStill previewWebp;
-//            private DownsizedSmall originalMp4;
-//            private FixedHeight fixedHeightSmall;
-//            private FixedHeight fixedHeight;
-//            private DownsizedSmall downsizedSmall;
-//            private DownsizedSmall preview;
-//            private FixedHeight fixedWidthDownsampled;
-//            private The480_WStill fixedWidthSmallStill;
-//            private FixedHeight fixedWidthSmall;
-//            private The480_WStill originalStill;
-//            private The480_WStill fixedWidthStill;
-//            private Looping looping;
-//            private FixedHeight fixedWidth;
-//            private The480_WStill previewGIF;
-//            private The480_WStill the480WStill;
-//        }
-//    }
-//
-//}
 
 
 
