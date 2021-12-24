b5e2bbe9b7a51a433ace65accb5f830b9449e1c7
Damir Kurtaev <damir.kurtaev@yandex.ru>
2021-12-24 23:27:50 +0300

    CurrencyServiceClientTest now works properly

--- src/test/java/per/ami/alpha/client/CurrencyServiceClientTest.java
+++ src/test/java/per/ami/alpha/client/CurrencyServiceClientTest.java
@@ -10,20 +10,19 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.util.StreamUtils;
 import per.ami.alpha.pojo.dto.Currency;
-import wiremock.org.apache.commons.io.IOUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.http.HttpResponse;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWireMock(port = 9091)
 class CurrencyServiceClientTest {
@@ -35,8 +34,8 @@ class CurrencyServiceClientTest {
 
     @Test
     void getLastCurrencies() {
-
-        stubFor(get(urlEqualTo("/latest.json?app_id=${app.feign.config.token.currency}"))
+        stubFor(get(urlPathEqualTo("/latest.json"))
+                .withQueryParam("app_id", matching(".*"))
                 .willReturn(aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
@@ -45,15 +44,31 @@ class CurrencyServiceClientTest {
         Currency currency = client.getLastCurrencies();
         assertThat(currency.getBase()).isEqualTo("USD");
         assertThat(currency.getRates()).isNotNull();
-        assertThat(currency.getRates().get("AED")).isCloseTo(3.67294f, Percentage.withPercentage(10));
+        assertThat(currency.getRates().get("AED")).isCloseTo(400.67294f, Percentage.withPercentage(10));
+    }
+
+    @Test
+    void getYesterdayCurrencies() {
+        String pattern = "yyyy-MM-dd";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern, Locale.ENGLISH);
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+
+        stubFor(get(urlPathEqualTo("/historical/" + yesterday.format(formatter) + ".json"))
+                .withQueryParam("app_id", matching(".*"))
+                .willReturn(aResponse()
+                        .withStatus(HttpStatus.OK.value())
+                        .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+                        .withBody(asJson(currencies))));
+
+
+        Currency currency = client.getYesterdayCurrencies(formatter.format(yesterday));
+        assertThat(currency.getBase()).isEqualTo("USD");
+        assertThat(currency.getRates()).isNotNull();
+        assertThat(currency.getRates().get("AED")).isCloseTo(400.67294f, Percentage.withPercentage(10));
     }
 
     @SneakyThrows
     private String asJson(Resource resource) {
         return StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
-    }
-
-    @Test
-    void getYesterdayCurrencies() {
     }
 }
--- src/test/java/per/ami/alpha/pojo/dto/CurrencyTestData.java
+++ src/test/java/per/ami/alpha/pojo/dto/CurrencyTestData.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 public class CurrencyTestData {
 
@@ -13,15 +12,6 @@ public class CurrencyTestData {
         rates.put("RATE1", 1f);
         rates.put("RATE2", 2f);
         rates.put("RATE3", 3f);
-//        Currency currency = new Currency();
-//        currency.setDisclaimer("disclaimer");
-//        currency.setLicense("license");
-//        currency.setTimestamp(123L);
-//        currency.setBase("base");
-//        currency.setRates(rates);
-//        return currency;
-//        Long f = new Long(1);
-//        new Currency("d", "l", f, "b", rates);
 
         return new Currency("disclaimer", "license", 123L, "base", rates);
     }
