d89af434e36b148dbf5e45dc8b84d4c1bd83fd34
Damir Kurtaev <damir.kurtaev@yandex.ru>
2021-12-24 23:36:48 +0300

    MyDateFormatter test added

---
+++ src/test/java/per/ami/alpha/utils/MyDateFormatterTest.java
@@ -0,0 +1,24 @@
+package per.ami.alpha.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyDateFormatterTest {
+
+    @Test
+    void getYesterday() {
+        MyDateFormatter myDateFormatter = new MyDateFormatter();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
+
+        assertThat(yesterday.format(formatter)).isEqualTo(myDateFormatter.getYesterday());
+
+    }
+}
